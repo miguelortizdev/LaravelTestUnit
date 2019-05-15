49a77d782f66d31f4010a384e88e43c854dc886d
Miguel Ortiz <miguelortiz1994@outlook.com>
2019-05-15 09:07:16 -0500

    1.2 Instalación y configuración del proyecto 1) php artisan make:auth 2) config/app name:Forum , timezone:America/Bogota, locale:es 3) php artisan migrate 4) Install StydeNet/html ( dependencia ) 5) Install Laravel Markdown ( dependencia ) 6) Comprobar funcionamiento comando: vendor/bin/phpuni

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -28,6 +28,9 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
+            /* StydeNet/html */
+            \Styde\Html\Alert\Middleware::class,
+
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
